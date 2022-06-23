1470a4cc8276b7459d764f96174bc62381173b42
n-pravab <23prabhav@gmail.com>
2022-06-23 17:43:02 +0545

    onmouse enter and leave event tested

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { useState } from "react";
-import { AiFillPlusCircle } from "react-icons/ai";
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
 // import { AAiFillMinusSquare } from "react-icons/aa";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
-  const [hovered, setHovered] = useState(false);
+  const [hovered, setHovered] = useState("");
   return (
     <div>
       <AiFillPlusCircle
         size={count + 20}
-        color={count >= 12 ? "red" : `${hovered ? "gold" : "green"}`}
+        color={count >= 12 ? "red" : `${hovered === "Plus" ? "gold" : "green"}`}
         onClick={() => setCount(count >= 12 ? count : count + 1)}
-        onMouseOver={() => setHovered(!hovered)}
+        // onMouseOver={(e) => setHovered("Plus")}
+        onMouseEnter={(e) => setHovered("Plus")}
+        onMouseLeave={(e) => setHovered("")}
       />
       <h1 style={{ color: `${count >= 12 ? "red" : "pink"}` }}>{count}</h1>
       {/* string vitra operation halna pare literal usee garne   */}
-      <button onClick={() => setCount(count > 1 ? count - 1 : count)}>-</button>
+      <AiFillMinusCircle
+        size={count + 20}
+        color={
+          count >= 12 ? "red" : `${hovered === "Minus" ? "gold" : "green"}`
+        }
+        onClick={() => setCount(count >= 12 ? count : count - 1)}
+        // onMouseOver={() => setHovered("Minus")}
+        onMouseEnter={() => setHovered("Minus")}
+        onMouseLeave={(e) => setHovered("")}
+      ></AiFillMinusCircle>
     </div>
   );
 };
