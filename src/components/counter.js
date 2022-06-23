ae7b01ac11109c77386864ede2f4d2dfb8b71324
n-pravab <23prabhav@gmail.com>
2022-06-23 17:18:52 +0545

    test hover event

--- src/components/counter.js
+++ src/components/counter.js
@@ -5,12 +5,14 @@ import { AiFillPlusCircle } from "react-icons/ai";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+  const [hovered, setHovered] = useState(false);
   return (
     <div>
       <AiFillPlusCircle
         size={count + 20}
-        color={count >= 12 ? "red" : "green"}
+        color={count >= 12 ? "red" : `${hovered ? "gold" : "green"}`}
         onClick={() => setCount(count >= 12 ? count : count + 1)}
+        onMouseOver={() => setHovered(true)}
       />
       <h1 style={{ color: `${count >= 12 ? "red" : "pink"}` }}>{count}</h1>
       {/* string vitra operation halna pare literal usee garne   */}
