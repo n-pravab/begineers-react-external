1863d903aef90d7fa981c2470202f6c1043bbff5
n-pravab <23prabhav@gmail.com>
2022-06-23 16:52:58 +0545

    test differents components on icon in counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { useState } from "react";
+import { AiFillPlusCircle } from "react-icons/ai";
+// import { AAiFillMinusSquare } from "react-icons/aa";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <h1>{count}</h1>
+      <AiFillPlusCircle
+        size={count + 20}
+        color={count >= 12 ? "red" : "green"}
+        onClick={() => setCount(count >= 12 ? count : count + 1)}
+      />
+      <h1 style={{ color: `${count >= 12 ? "red" : "pink"}` }}>{count}</h1>
+      {/* string vitra operation halna pare literal usee garne   */}
       <button onClick={() => setCount(count > 1 ? count - 1 : count)}>-</button>
     </div>
   );
